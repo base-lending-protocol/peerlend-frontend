47c43b50f6c5dc9546a3ee62aad315098362cabe
PhantomOz <faniogor@gmail.com>
2024-06-30 22:59:35 +0100

    adds onchainkit identity

--- src/utility/WalletConnected.jsx
+++ src/utility/WalletConnected.jsx
@@ -1,15 +1,19 @@
+import { Avatar, Identity, Name, Badge, Address } from '@coinbase/onchainkit/identity';
+
 const WalletConnected = ({ address, icon }) => {
-    const formatAddress = (address) => {
-        return `${address?.slice(0, 6)}...${address?.slice(-4)}`
-    }
-    return (
-        <span className="flex items-center gap-1">
-            <span className="w-6 h-6 rounded-full overflow-hidden">
-                <img src={icon} alt="Icon" className="w-full h-full object-cover" />
-            </span>
-            <span className="text-gray-200">{formatAddress(address)}</span>
-        </span>
+    return( 
+    <Identity className='bg-transparent' address={address} schemaId={'0xf8b05c79f090979bf4a80270aba232dff11a10d9ca55c4f88de95317970f0de9'}>
+       <Avatar src={icon} alt='icon'> 
+            <Badge className="bg-error" />
+        </Avatar>
+        <Name className="text-black" />
+        <Address className="text-white font-bold"/>
+    </Identity>
     )
+}
+WalletConnected.propTypes = {
+    address: String,
+    icon: String
 }
 
 export default WalletConnected;
